56df9ab5e7d9c8f63dbe5e970673c42427a075b0
The-Blight <hezer.fil@gmail.com>
2026-07-03 01:34:22 +0300

    Add 'Unit' readonly struct to represent void

---
+++ src/Fx.Net/Unit/Unit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Fx.Net.Unit;
+
+[SkipLocalsInit]
+[StructLayout(LayoutKind.Sequential, Size = 1)]
+public readonly struct Unit : IEquatable<Unit>, IComparable<Unit>, IComparable
+{
+    private static readonly Unit _value = new();
+    public static ref readonly Unit Value => ref _value;
+
+    public static Task<Unit> SuccessfulTask { get; } = Task.FromResult(_value);
+    public static readonly ValueTask<Unit> SuccessfulValueTask = new(_value);
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Equals(Unit other) => true;
+
+    public override bool Equals(object? obj) => obj is Unit;
+    public override int GetHashCode() => 0;
+
+    public int CompareTo(Unit other) => 0;
+    public int CompareTo(object? obj) => 0;
+
+    public static bool operator ==(Unit left, Unit right) => true;
+    public static bool operator !=(Unit left, Unit right) => false;
+    public override string ToString() => "()";
+}
