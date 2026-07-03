49fa5a40f03bd86f9e31902a374cba2bbefda857
The-Blight <hezer.fil@gmail.com>
2026-07-03 21:01:16 +0300

    feat: Define common 'Errors' instances for Result

---
+++ src/Fx.Net/Errors/ResultErrors.cs
@@ -0,0 +1,31 @@
+﻿using Fx.Net.Types;
+
+namespace Fx.Net.Errors;
+
+/// <summary>
+///     Набор предопределенных объектов ошибок для работы c контейнером <see cref="Result"/>
+/// </summary>
+public static class ResultErrors
+{
+    /// <summary>
+    ///     Ошибка, указывающая на прерывание цепочки вычислений из-за возврата некорректного или пустого состояния.
+    /// </summary>
+    public static readonly Error NullResult = new(
+        "Result.ChainBroken",
+        "Цепочка вычислений прервана: один из этапов вернул пустое или некорректное состояние.");
+
+
+    /// <summary>
+    ///     Ошибка, возникающая при попытке инициализировать успешный результат значением <see langword="null"/>.
+    /// </summary>
+    public static readonly Error NullValue = new(
+        "Result.NullValue",
+        "Попытка инициализировать успешный результат null значением.");
+
+    /// <summary>
+    ///     Ошибка, указывающая на обращение к неинициализированному экземпляру Result, созданному через <c>default</c>.
+    /// </summary>
+    public static readonly Error DefaultNullFailure = new(
+        "Result.InvalidInitialization",
+        "Использование неинициализированного объекта Result, созданного через 'default'.");
+}
