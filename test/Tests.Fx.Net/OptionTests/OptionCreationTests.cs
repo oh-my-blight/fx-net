f513f3e4b409f8198316b2957b512f6a0fa83b7c
The-Blight <hezer.fil@gmail.com>
2026-07-10 18:21:55 +0300

    test: add unit tests for Option monad
    - Verify equality logic.
    - Test factory methods (Some/None).
    - Ensure correct instance state management.

---
+++ test/Tests.Fx.Net/OptionTests/OptionCreationTests.cs
@@ -0,0 +1,47 @@
+﻿using Fx.Net.Monads.Option;
+
+namespace Tests.Fx.Net.OptionTests;
+
+public class OptionCreationTests
+{
+    [Fact]
+    public void Some_WhenValueIsNotNull_HasValueIsTrueAndIsNoneIsFalse()
+    {
+        var expectedValue = 20;
+
+        var option = Option.Some(expectedValue);
+
+        Assert.True(option.HasValue);
+        Assert.False(option.IsNone);
+    }
+
+
+    [Fact]
+    public void Some_WhenValueIsNull_IsNoneIsTrueAndHasValueIsFalse()
+    {
+        string expectedValue = null!;
+
+        var option = Option.Some(expectedValue);
+
+        Assert.True(option.IsNone);
+        Assert.False(option.HasValue);
+    }
+
+    [Fact]
+    public void None_WhenAssigned_IsNoneIsTrueAndHasValueIsFalse()
+    {
+        Option<string> option = Option.None;
+
+        Assert.True(option.IsNone);
+        Assert.False(option.HasValue);
+    }
+
+    [Fact]
+    public void Default_WhenCreatedAsDefaultStruct_IsNoneIsTrue_And_HasValueIsFalse()
+    {
+        Option<int> option = default;
+
+        Assert.True(option.IsNone);
+        Assert.False(option.HasValue);
+    }
+}
---
+++ test/Tests.Fx.Net/OptionTests/OptionEqualityTests.cs
@@ -0,0 +1,100 @@
+﻿using Fx.Net.Monads.Option;
+
+namespace Tests.Fx.Net.OptionTests;
+
+public class OptionEqualityTests
+{
+    [Fact]
+    public void Equals_TwoSomeWithSamePrimitiveValues_ReturnsTrue()
+    {
+        const int expectedValue = 42;
+        var firstOption = Option.Some(expectedValue);
+        var secondOption = Option.Some(expectedValue);
+
+        Assert.Multiple(
+            () => Assert.True(firstOption.Equals(secondOption)),
+            () => Assert.True(firstOption.Equals((object)secondOption)),
+            () => Assert.True(firstOption == secondOption),
+            () => Assert.False(firstOption != secondOption),
+            () => Assert.Equal(firstOption.GetHashCode(), secondOption.GetHashCode())
+        );
+    }
+
+
+    [Fact]
+    public void Equals_TwoSomeWithDifferentPrimitiveValues_ReturnsFalse()
+    {
+        const int firstNum = 25;
+        const int secondNum = 99;
+        var firstOption = Option.Some(firstNum);
+        var secondOption = Option.Some(secondNum);
+
+        Assert.Multiple(
+            () => Assert.False(firstOption.Equals(secondOption)),
+            () => Assert.False(firstOption.Equals((object)secondOption)),
+            () => Assert.False(firstOption == secondOption),
+            () => Assert.True(firstOption != secondOption),
+            () => Assert.NotEqual(firstOption.GetHashCode(), secondOption.GetHashCode())
+        );
+    }
+
+    [Fact]
+    public void Equals_TwoSomeWithContentEqualReferenceTypes_ReturnsTrue()
+    {
+        var firstStr = new string(new[] { 'a', 'b', 'c' });
+        var secondStr = new string(new[] { 'a', 'b', 'c' });
+
+        var firstOption = Option.Some(firstStr);
+        var secondOption = Option.Some(secondStr);
+
+        Assert.False(ReferenceEquals(firstStr, secondStr));
+        Assert.True(firstOption.Equals(secondOption));
+        Assert.Equal(firstOption.GetHashCode(), secondOption.GetHashCode());
+    }
+
+
+    [Fact]
+    public void Equals_ExplicitNoneAndDefaultStruct_ReturnsTrue()
+    {
+        Option<string> noneExplicit = Option.None;
+        Option<string> noneDefault = default;
+
+        Assert.True(noneExplicit.Equals(noneDefault));
+        Assert.True(noneExplicit == noneDefault);
+        Assert.Equal(noneExplicit.GetHashCode(), noneDefault.GetHashCode());
+    }
+
+    [Fact]
+    public void Equals_SomeAndNone_ReturnsFalse()
+    {
+        var someOption = Option.Some("Value");
+        Option<string> noneOption = Option.None;
+
+        Assert.False(someOption.Equals(noneOption));
+        Assert.False(noneOption.Equals(someOption));
+        Assert.True(someOption != noneOption);
+    }
+
+    [Fact]
+    public void Equals_SomeWithDefaultValueDefaultStruct_ReturnsFalse()
+    {
+        var someZero = Option.Some(0);
+        Option<int> noneDefault = default;
+
+        Assert.False(someZero.Equals(noneDefault));
+        Assert.True(someZero != noneDefault);
+    }
+
+
+    [Fact]
+    public void Equals_WithNullOrDifferentTypeObject_ReturnsFalse()
+    {
+        const string value = "Value";
+        const string testValue = "10";
+        var option = Option.Some(10);
+
+        Assert.False(option.Equals((object?)null));
+        Assert.False(option.Equals(value));
+        Assert.False(option.Equals(Option.Some(testValue)));
+    }
+}
