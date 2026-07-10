409f5a76cd8f1bf19dc399ecb2833444c6eedeff
The-Blight <hezer.fil@gmail.com>
2026-07-10 18:11:21 +0300

    feat: add enum for monadic state

---
+++ src/Fx.Net/Monads/State.cs
@@ -0,0 +1,22 @@
+﻿namespace Fx.Net.Monads;
+
+/// <summary>
+///     Состояние контектса внутри монады
+/// </summary>
+public enum State : byte
+{
+    /// <summary>
+    ///     Обозначает отсуствие значения
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    ///     Обозначает состояние присуствия значение
+    /// </summary>
+    Some = 1,
+
+    /// <summary>
+    ///     Обозначает состояние ошибки монады 
+    /// </summary>
+    Failure = 2
+}
